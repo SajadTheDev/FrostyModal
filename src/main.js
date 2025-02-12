52016b2d9b42343afabab634b6151a5842d4983d
saji021 <sajmhhhhh67@gmail.com>
2025-02-12 17:44:07 +0330

    feat(styles): integrate Tailwind CSS and add comments for better clarity

--- src/main.js
+++ src/main.js
@@ -1,24 +1,33 @@
 'use strict';
 import './style.css';
+// Select necessary elements
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// Function to open the modal
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
+
+// Function to close the modal
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
 
+// Add event listeners to open modal buttons
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+// Add event listeners to close modal
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
-document.addEventListener('keydown', function (event) {
-  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+
+// Close modal when pressing the Escape key
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
